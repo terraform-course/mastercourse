71cb8465d1cb5b9ba8243203a55974a1c439b8b1
Alex Tasioulis <terraformcourse@gmail.com>
2021-01-24 18:41:21 +0000

    add bucket and dynamodb table for state, configure s3 backend

--- aws/main.tf
+++ aws/main.tf
@@ -1,7 +1,7 @@
 module "grafana" {
   source = "./grafana-server"
 
-  instance_type = "t3.small"
+  instance_type = "t3.micro"
 }
 
 module "cloudwatch" {
@@ -9,4 +9,41 @@ module "cloudwatch" {
 
   grafana_ip = module.grafana.ip
   grafana_password = module.grafana.password
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-course-example-bucket"
+    key    = "infrastructure/monitoring/grafana/terraform.tfstate"
+    region = "eu-west-1"
+    dynamodb_table = "terraform-state"
+  }
+}
+
+resource "aws_s3_bucket" "state" {
+  bucket = "terraform-course-example-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "terraform-course-example-bucket"
+  }
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_dynamodb_table" "state" {
+  name           = "terraform-state"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "terraform-state"
+  }
 }
